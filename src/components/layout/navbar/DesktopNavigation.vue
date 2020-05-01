5fd9ef1a9094ffc97bfe529fffd2e32de720edb1
TNORTHERN <traynorthern@yahoo.com>
2020-05-01 17:17:20 -0500

    removing desktop nav on mobile and fixing overflow

--- src/components/layout/navbar/DesktopNavigation.vue
+++ src/components/layout/navbar/DesktopNavigation.vue
@@ -1,19 +1,21 @@
 <template>
   <div :style="`width: 100%;background-color: ${$store.state.constants.colors.darkerBlue};`">
-    <v-row
-      justify="center"
-      style="padding: 25px 0; color: white: font-size: 40px;"
-    >
-      <v-col :cols="cols">
-        <p>test</p>
-      </v-col>
-      <v-col :cols="cols">
-        <p>test</p>
-      </v-col>
-      <v-col :cols="cols">
-        <p>tests</p>
-      </v-col>
-    </v-row>
+    <v-container>
+      <v-row
+        justify="center"
+        class="desktop-navigation"
+      >
+        <v-col :cols="cols">
+          <p>test</p>
+        </v-col>
+        <v-col :cols="cols">
+          <p>test</p>
+        </v-col>
+        <v-col :cols="cols">
+          <p>tests</p>
+        </v-col>
+      </v-row>
+    </v-container>
   </div>
 </template>
 
@@ -27,9 +29,18 @@ export default {
 }
 </script>
 
-<style>
+<style lang="scss" scoped>
+@import "@/assets/styles";
 p {
   color: white;
   font-size: 30px;
+}
+.desktop-navigation {
+  padding: 10px 0;
+  color: white;
+  font-size: 40px;
+  @include mobile() {
+    display: none;
+  }
 }
 </style>
