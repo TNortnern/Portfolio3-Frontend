d42b40d6b03dba3e5e411136be579cb95375aa0c
TNORTHERN <traynorthern@yahoo.com>
2020-05-02 21:10:35 -0500

    animation update

--- src/components/portfolio/projects/ProjectCards.vue
+++ src/components/portfolio/projects/ProjectCards.vue
@@ -4,6 +4,7 @@
       name="zoom"
       tag="div"
       class="row justify-center"
+      @before-leave="beforeLeave"
     >
       <ProjectCard
         v-for="project in projects"
@@ -35,6 +36,14 @@ export default {
     currentFilter () {
       return this.$store.state.projects.items[this.$store.state.projects.tab.name].tab
     }
+  },
+  methods: {
+    beforeLeave ($el) {
+      // Leave in place.
+      const top = $el.offsetTop + 'px'
+      const left = $el.offsetLeft + 'px'
+      Object.assign($el.style, { left, top })
+    },
   }
 }
 </script>
@@ -45,27 +54,24 @@ export default {
 }
 
 .fade-enter-active {
-  transition: all .4s;
+  transition: all 0.4s;
 }
-
-
 
 // NEED TO ADD POSITION: ABSOLUTE TO THE ELEMENT FOR REMOVAL
 .fade-leave-active {
-  transition: all .3s;
+  transition: all 0.3s;
   opacity: 0;
   transform: translateY(0);
   position: absolute;
 }
 
 .fade-move {
-  transition: all .5s cubic-bezier(0.77, 0, 0.175, 1);
+  transition: all 0.5s cubic-bezier(0.77, 0, 0.175, 1);
   @for $i from 1 through 15 {
     &:nth-child(#{$i}) {
-       transition: transform .3s #{$i * 0.1}s cubic-bezier(0.77, 0, 0.175, 1);
+      transition: transform 0.3s #{$i * 0.1}s cubic-bezier(0.77, 0, 0.175, 1);
     }
   }
-
 }
 
 .zoom-enter-active,
@@ -73,27 +79,23 @@ export default {
   animation-duration: 0.5s;
   animation-fill-mode: both;
   animation-name: zoom;
-    transition: all .4s;
-
+  transition: all 0.4s;
 }
 
 .zoom-leave-active {
   animation-direction: reverse;
-  transition: all .4s ease;
-  opacity: 0;
-//   transform: translateY(0);
+  transition: all 0.4s ease;
+  // opacity: 0;
   position: absolute;
 }
 
-
 .zoom-move {
-  transition: all .5s cubic-bezier(0.77, 0, 0.175, 1);
-//   @for $i from 1 through 15 {
-//     &:nth-child(#{$i}) {
-//        transition: transform .3s #{$i * 0.1}s cubic-bezier(0.77, 0, 0.175, 1);
-//     }
-//   }
-
+  transition: all 0.5s cubic-bezier(0.77, 0, 0.175, 1);
+  //   @for $i from 1 through 15 {
+  //     &:nth-child(#{$i}) {
+  //        transition: transform .3s #{$i * 0.1}s cubic-bezier(0.77, 0, 0.175, 1);
+  //     }
+  //   }
 }
 
 @keyframes zoom {
@@ -106,5 +108,4 @@ export default {
     opacity: 1;
   }
 }
-
 </style>
